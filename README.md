fc7426d4c11af9c230d1b850820c835a78c3950c
shawank <shawank17198@gmail.com>
2019-05-24 19:19:57 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-xgddia
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-xgddia)
---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {GameService} from './game.service'
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular';
+
+  constructor(private service:GameService){}
+
+  
+}
+safjslkdjf
